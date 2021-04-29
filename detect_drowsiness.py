bc1ff66cb01f279e083aba8d645bb0ec4d38de01
chocochip13 <kapeleshh@gmail.com>
2021-04-29 19:03:06 +0530

    change Videocapture to openCV from imutils

--- detect_drowsiness.py
+++ detect_drowsiness.py
@@ -63,14 +63,14 @@ predictor = dlib.shape_predictor(args["shape_predictor"])
 
 # start the video stream thread
 print("[INFO] starting video stream thread...")
-vs = VideoStream(src=args["webcam"]).start()
+vs = cv2.VideoCapture(0)
 time.sleep(1.0)
 # loop over frames from the video stream
 while True:
     # grab the frame from the threaded video file stream, resize
     # it, and convert it to grayscale
     # channels)
-    frame = vs.read()
+    ret, frame = vs.read()
     frame = imutils.resize(frame, width=450)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # detect faces in the grayscale frame
